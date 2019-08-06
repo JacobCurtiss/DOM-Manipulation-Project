7e8b63cf0fdd7b11da63b7a174ba7b7716607b90
Jacob <jcurtiss94@outlook.com>
2019-08-05 21:30:28 -0400

    Adjustments and began js file
    Adjusted the html and css to add styling, readability, and font
    
    added some button functionality
    
    performed some tweaks to become more inline with best practices
    
    finished the basic button functions

---
+++ DOMproject.js
@@ -0,0 +1,153 @@
+//Variable declaration section
+
+const html = document.documentElement;
+
+//text divs
+const para = document.getElementById("paragraphs");
+
+const lastpara = document.getElementById("lastpara");
+
+const firstpara = document.getElementById("explain");
+
+const heading = document.getElementById("header");
+
+//btns
+
+const mainBtn = document.getElementById("original");
+
+const redBtn = document.getElementById("red");
+
+const orangeBtn = document.getElementById("orange");
+
+const yellowBtn = document.getElementById("yellow");
+
+const violetBtn = document.getElementById("violet");
+
+const blueBtn = document.getElementById("blue");
+
+const orchidBtn = document.getElementById("orchid");
+
+const sunflowerBtn = document.getElementById("sunflower");
+
+const neonBtn = document.getElementById("neon");
+
+const original = function() {
+    document.body.background = "https://images.unsplash.com/photo-1470198002244-184c9f5da03f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80";
+    header.style.color = "black";
+    firstpara.style.color = "black";
+    para.style.color = "black";
+    lastpara.style.color = "whitesmoke";
+    firstpara.style.textShadow = "white 1px 1px 1px";
+    para.style.textShadow = "white 1px 1px 1px";
+    lastpara.style.textShadow = "black 10px 10px 10px";
+    document.body.height = "100% vh";
+
+}
+
+const red = function() {
+    document.body.background = "https://images.unsplash.com/photo-1535868463750-c78d9543614f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1655&q=80 no-repeat center center fixed";
+    header.style.color = "red";
+    firstpara.style.color = "red";
+    para.style.color = "red";
+    lastpara.style.color = "red";
+    firstpara.style.textShadow = "green 1px 1px 1px";
+    para.style.textShadow = "green 1px 1px 1px";
+    lastpara.style.textShadow = "green 1px 1px 1px";
+    document.body.height = "100% vh";
+}
+
+const orange = function() {
+    document.body.background = "https://images.unsplash.com/photo-1523730205978-59fd1b2965e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1891&q=80 no-repeat center center fixed";
+    header.style.color = "orange";
+    firstpara.style.color = "orange";
+    para.style.color = "orange";
+    lastpara.style.color = "orange";
+    firstpara.style.textShadow = "blue 1px 1px 1px";
+    para.style.textShadow = "blue 1px 1px 1px";
+    lastpara.style.textShadow = "blue 1px 1px 1px";
+    document.body.height = "100% vh";
+}
+
+const yellow = function() {
+    document.body.background = "https://images.unsplash.com/photo-1533891244820-1d2dbcf93f56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80";
+    header.style.color = "yellow";
+    firstpara.style.color = "yellow";
+    para.style.color = "yellow";
+    lastpara.style.color = "yellow";
+    firstpara.style.textShadow = "blue 1px 1px 1px";
+    para.style.textShadow = "blue 1px 1px 1px";
+    lastpara.style.textShadow = "blue 1px 1px 1px";
+    document.body.height = "100% vh";
+}
+
+const violet = function() {
+    document.body.background = "https://images.unsplash.com/photo-1516889782132-fb580815f8f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80";
+    header.style.color = "violet";
+    firstpara.style.color = "violet";
+    para.style.color = "violet";
+    lastpara.style.color = "violet";
+    firstpara.style.textShadow = "yellow 1px 1px 1px";
+    para.style.textShadow = "yellow 1px 1px 1px";
+    lastpara.style.textShadow = "yellow 10px 10px 10px";
+    document.body.height = "100% vh";
+}
+
+const blue = function() {
+    document.body.background = "https://images.unsplash.com/photo-1514161740820-6926c370d87e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1875&q=80";
+    header.style.color = "blue";
+    firstpara.style.color = "blue";
+    para.style.color = "blue";
+    lastpara.style.color = "blue";
+    firstpara.style.textShadow = "purple 1px 1px 1px";
+    para.style.textShadow = "purple 1px 1px 1px";
+    lastpara.style.textShadow = "purple 1px 1px 1px";
+    document.body.height = "100% vh";
+}
+
+const orchid = function() {
+    document.body.background = "https://images.unsplash.com/photo-1527904248767-cfe951e81ab0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80";
+    header.style.color = "violet";
+    firstpara.style.color = "violet";
+    para.style.color = "violet";
+    lastpara.style.color = "violet";
+    firstpara.style.textShadow = "black 1px 1px 1px";
+    para.style.textShadow = "black 1px 1px 1px";
+    lastpara.style.textShadow = "black 1px 1px 1px";
+    document.body.height = "100% vh";
+}
+
+const sunflower = function() {
+    document.body.background = "https://images.unsplash.com/photo-1481595357459-84468f6eeaac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80";
+    header.style.color = "yellow";
+    firstpara.style.color = "yellow";
+    para.style.color = "yellow";
+    lastpara.style.color = "yellow";
+    firstpara.style.textShadow = "blue 1px 1px 1px";
+    para.style.textShadow = "blue 1px 1px 1px";
+    lastpara.style.textShadow = "blue 10px 10px 10px";
+    document.body.height = "100% vh";
+}
+
+const neon = function() {
+    document.body.background = "https://images.unsplash.com/photo-1561882458-3b5d03e4b4aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80";
+    header.style.color = "#39ff14";
+    firstpara.style.color = "#39ff14";
+    para.style.color = "#39ff14";
+    lastpara.style.color = "#39ff14";
+    firstpara.style.textShadow = "#bc13fe 1px 1px 1px";
+    para.style.textShadow = "#bc13fe 1px 1px 1px";
+    lastpara.style.textShadow = "black 1px 1px 1px";
+    document.body.height = "100% vh";
+}
+
+
+
+mainBtn.addEventListener("click", original)
+redBtn.addEventListener("click", red);
+orangeBtn.addEventListener("click", orange);
+yellowBtn.addEventListener("click", yellow);
+violetBtn.addEventListener("click", violet);
+blueBtn.addEventListener("click", blue);
+orchidBtn.addEventListener("click", orchid);
+sunflowerBtn.addEventListener("click", sunflower);
+neonBtn.addEventListener("click", neon);
